9f0ebe1320a11122af299ec9e7589d7c61338ead
User <denchiksrty@gmail.com>
2023-11-25 23:05:58 +0400

    day3nuight

--- app/src/main/java/com/example/vp/BuisnessFragment.java
+++ app/src/main/java/com/example/vp/BuisnessFragment.java
@@ -1,22 +1,19 @@
 package com.example.vp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
-public class AllFragment extends Fragment {
+import androidx.fragment.app.Fragment;
 
-    public AllFragment(){
+public class BuisnessFragment extends Fragment {
+
+    public BuisnessFragment(){
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v =  inflater.inflate(R.layout.fragment_all, container, false);
-        return v;
+        return inflater.inflate(R.layout.fragment_buisness, container, false);
     }
 }
